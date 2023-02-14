96a15806c8a44b643f6df37f3bbbf71aac618fd7
Modernakash <pawarakash162000@gmail.com>
2023-02-14 10:50:59 +0530

    first Commit

--- scraper.py
+++ scraper.py
@@ -112,4 +112,4 @@ DRIVER_PATH = r'C:\all_project\scrapper\ImageScrapper\chromedriver.exe'
 search_term = 'Modernakash'
 # num of images you can pass it from here  by default it's 10 if you are not passing
 #number_images = 50
-search_and_download(search_term=search_term, driver_path=DRIVER_PATH, number_images=150)
+search_and_download(search_term=search_term, driver_path=DRIVER_PATH, number_images=50)
